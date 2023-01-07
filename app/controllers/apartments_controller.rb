9413ce61c1fdca8d57afc30544b399c0905e00a1
manny-espinoza <mannyespinoza123@yahoo.com>
2023-01-06 16:04:49 -0800

    Fixed changes and removed said items

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,12 +1,2 @@
 class ApartmentsController < ApplicationController
-
-    def create
-        apartment = Apartment.create(apartment_params)
-        if apartment.valid?
-            render json: apartment
-        else
-            render json: apartment.errors, status: 422
-        end
-    end
-
 end
