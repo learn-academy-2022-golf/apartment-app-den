2c15f7c661f15ac2367de4b16a0163dfc5942d2b
TKash89 <tyler.kasheta@outlook.com>
2023-01-09 13:16:39 -0800

    homepage branch complete

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,7 +3,8 @@ import React from "react"
 const Home = () => {
   return (
     <>
-      <h3>Home</h3>
+      <h3>Welcome to Den Apartments</h3>
+      <p>An extensive list of Apartments to meet the needs of rental seekers alike.</p>
     </>
   )
 }
