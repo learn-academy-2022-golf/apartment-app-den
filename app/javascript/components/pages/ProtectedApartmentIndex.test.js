d99f9e4141328077348d637f8f8de92fa6b7abbb
TKash89 <tyler.kasheta@outlook.com>
2023-01-10 15:20:51 -0800

    Added customized Jest testing

--- app/javascript/components/pages/ProtectedApartmentIndex.test.js
+++ app/javascript/components/pages/ProtectedApartmentIndex.test.js
@@ -1,11 +1,24 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import ProtectedApartmentIndex from "./ProtectedApartmentIndex"
+import { BrowserRouter } from "react-router-dom"
+import Navigation from "../components/Navigation"
+import userEvent from "@testing-library/user-event"
 
 describe("<ProtectedApartmentIndex />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
     render(<ProtectedApartmentIndex />, div)
   })
-  
+  it("has functional navigation links", () => {
+    render(
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
+    )
+    userEvent.click(screen.getByText("Home"))
+    expect(screen.getByText("Home")).toBeInTheDocument()
+    userEvent.click(screen.getByText("All Apartments"))
+    expect(screen.getByText("All Apartments")).toBeInTheDocument()
+   })
 })
