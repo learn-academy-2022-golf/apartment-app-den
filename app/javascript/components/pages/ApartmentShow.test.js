3edd391fa992d34fefe7701f4d22642180eeea2e
Shadow090509 <jamoso090509@gmail.com>
2023-01-11 12:29:28 -0800

    push

--- app/javascript/components/pages/ApartmentShow.test.js
+++ app/javascript/components/pages/ApartmentShow.test.js
@@ -17,31 +17,3 @@ describe("<ApartmentShow />", () => {
     );
   });
 });
-
-// -------------------------------------------
-
-// const renderComponent = (Apartments) => {
-
-//   render(
-//     <MemoryRouter initialEntries={["/apartmentshow/1"]}>
-//       <Routes>
-//           <Route path="apartmentshow/:id" element={<ApartmentShow apartments={Apartments} />}/>
-//       </Routes>
-//     </MemoryRouter>
-//   )
-// }
-
-// describe("<ApartmentShow />", () => {
-
-//   it("renders a card for each Apartment", (Apartments) => {
-
-//     render(
-//       <ApartmentShow apartments={Apartments} />
-//     );
-
-//     Apartments.map((apartment) => {
-//       const nameElement = screen.getByText(apartment.name);
-//       expect(nameElement).toBeInTheDocument();
-//     });
-//   });
-// });
