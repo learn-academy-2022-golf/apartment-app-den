fc2f82a1034003dac39fc185d658ed2021d11372
frankburgers <franciscoburgers@gmail.com>
2023-01-10 16:46:05 -0800

    saving

--- app/javascript/components/pages/ApartmentShow.test.js
+++ app/javascript/components/pages/ApartmentShow.test.js
@@ -1,10 +1,30 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import ApartmentShow from "./ApartmentShow"
+import { MemoryRouter, NavLink, Routes, Route } from "react-router-dom"
+
+const renderComponent = (Apartments) => {
+
+  render(
+    <MemoryRouter initialEntries={["/apartmentshow/1"]}>
+      <Routes>
+          <Route path="apartmentshow/:id" element={<ApartmentShow apartments={Apartments} />}/>
+      </Routes>
+    </MemoryRouter>
+  )
+}
 
 describe("<ApartmentShow />", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div")
-    render(<ApartmentShow />, div)
-  })
-})
+
+  it("renders a card for each Apartment", (Apartments) => {
+
+    render(
+      <ApartmentShow apartments={Apartments} />
+    );
+
+    Apartments.map((apartment) => {
+      const nameElement = screen.getByText(apartment.name);
+      expect(nameElement).toBeInTheDocument();
+    });
+  });
+});
